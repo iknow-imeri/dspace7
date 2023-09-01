83e37211e32844e8cb89865e3d3dde732d450f0e
iknow-imeri <iknow.imeri@gmail.com>
2023-09-01 17:49:18 +0700

    unclonned

--- dspace_angular/src/app/core/cache/models/request-param.model.ts
+++
@@ -1,9 +0,0 @@
-
-/**
- * Class representing a query parameter (query?fieldName=fieldValue) used in FindListOptions object
- */
-export class RequestParam {
-  constructor(public fieldName: string, public fieldValue: any) {
-
-  }
-}
